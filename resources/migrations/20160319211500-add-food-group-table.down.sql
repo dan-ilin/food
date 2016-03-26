8a6306cf81437cce5eccb650e20d7c7ded51dfbf
Dan Ilin <dan.ilin@me.com>
2016-03-26 00:29:49 -0400

    Add migration scripts to populate db.

---
+++ resources/migrations/20160319211500-add-food-group-table.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE "food_group";
---
+++ resources/migrations/20160319211503-add-common-nutrient-table.up.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "common_nutrient" (
+  id INT PRIMARY KEY REFERENCES nutrient (id)
+);
+--;;
+INSERT INTO "common_nutrient" (id) VALUES
+  (203),
+  (204),
+  (205),
+  (208),
+  (269),
+  (291),
+  (301),
+  (303),
+  (304),
+  (305),
+  (306),
+  (307),
+  (309),
+  (317),
+  (318),
+  (324),
+  (401),
+  (404),
+  (405),
+  (406),
+  (415),
+  (418),
+  (430),
+  (431),
+  (601);
