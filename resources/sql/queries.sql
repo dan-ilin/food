e8f43e0490bd1eca8c369552e1094b5d9cab4939
Dan Ilin <dan.ilin@me.com>
2016-03-27 19:44:45 -0400

    Add basic select queries for food, food group, and nutrition data.

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,21 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- :name get-food-groups :? :*
+-- :doc retrieve all food groups
+SELECT * FROM food_group;
 
--- :name update-user! :! :n
--- :doc update an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
+-- :name get-food-by-food-group :? :*
+-- :doc retrieve food by food group (with paging)
+SELECT * FROM food WHERE food_group_id = :food_group_id
+ORDER BY id LIMIT :limit OFFSET :offset;
 
--- :name get-user :? :1
--- :doc retrieve a user given the id.
-SELECT * FROM users
-WHERE id = :id
+-- :name get-nutrition-by-food :? :*
+-- :doc retrieve nutrition by food
+SELECT * FROM nutrition WHERE food_id = :food_id;
 
--- :name delete-user! :! :n
--- :doc delete a user given the id
-DELETE FROM users
-WHERE id = :id
+-- :name get-nutrients :? :*
+-- :doc retrieve nutrients
+SELECT * FROM nutrient;
+
+-- :name get-common-nutrients :? :*
+-- :doc retrieve common nutrients
+SELECT n.id, n.units, n.tagname, n.name, n.num_decimal_places, n.sr_order
+FROM common_nutrient AS c JOIN nutrient AS n ON c.id = n.id;
